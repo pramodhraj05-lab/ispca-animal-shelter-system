c14d15cca546c4592978ec044cba75aca367dbba
Pramodh S <20092022@mydbs.ie>
2026-03-22 13:00:01 +0000

    Removed unused in-memory data

--- backend/routes/animals.js
+++ backend/routes/animals.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/database");
 
-let animals = [
-  { id: 1, name: "Buddy", species: "Dog", age: 3 }
-];
 router.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
