a731117978a20b72657d2c4adcd3272247c40d77
Pramodh S <20092022@mydbs.ie>
2026-03-22 12:16:54 +0000

    Created animals table in SQLite database

--- backend/database.js
+++ backend/database.js
@@ -1,14 +1,8 @@
-const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
-
-const dbPath = path.join(__dirname, "animals.db");
-
-const db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error("Database connection failed", err);
-  } else {
-    console.log("Connected to SQLite database");
-  }
-});
-
-module.exports = db;
+db.run(`
+  CREATE TABLE IF NOT EXISTS animals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    species TEXT NOT NULL,
+    age INTEGER
+  )
+`); 
