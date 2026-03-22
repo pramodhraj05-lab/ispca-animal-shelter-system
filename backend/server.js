06deae1c8790820e5971f9fba51459bb67acb26c
Pramodh S <20092022@mydbs.ie>
2026-03-22 10:44:38 +0000

    Added express.json for incoming json

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path"); 
 const app = express();
 const animalRoutes = require("./routes/animals");
+app.use(express.json());
 app.use("/animals", animalRoutes);
 app.use(express.static(path.join(__dirname, "../frontend")));
 app.get("/", (req, res) => {
