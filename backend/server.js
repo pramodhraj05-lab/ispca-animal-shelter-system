fce23984ca3878853c2a1357621c5994d87e01b6
Pramodh S <20092022@mydbs.ie>
2026-03-25 12:32:33 +0000

    Added route to shelter and adpotion

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express = require("express");
-const path = require("path"); 
+const path = require("path");
+
 const app = express();
+
+app.use(express.json());
+
 const animalRoutes = require("./routes/animals");
 const shelterRoutes = require("./routes/shelters");
-app.use("/shelters", shelterRoutes);  
-app.use(express.json());
+const adoptionRoutes = require("./routes/adoptions");
+
 app.use("/animals", animalRoutes);
+app.use("/shelters", shelterRoutes);
+app.use("/adoptions", adoptionRoutes);
+
 app.use(express.static(path.join(__dirname, "../frontend")));
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
+
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
