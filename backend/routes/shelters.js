6eed7b591d1e1f0894173e6d70f060a550a3e6d5
Pramodh S <20092022@mydbs.ie>
2026-03-25 12:22:42 +0000

    Connecting route to shelter

---
+++ backend/routes/shelters.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db/database");
+
+router.get("/", (req, res) => {
+  db.all("SELECT * FROM shelters", [], (err, rows) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(rows);
+  });
+});
