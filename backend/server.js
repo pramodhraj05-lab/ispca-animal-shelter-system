e00ada39eae57107d5af194ac01887495f82722d
Pramodh S <20092022@mydbs.ie>
2026-04-10 10:47:43 +0100

    changes in code

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 
 // ── START ─────────────────────────────────────────
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`\n🐾 ISPCA running on http://localhost:${PORT}`);
   console.log(`   Admin login: admin@pawhaven.com / admin123\n`);
 });
